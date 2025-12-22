f4c667cc3706e9b0e72e3dc4e39dae654c38ee0b
Dimitris Nimas <hello@dimitrisnimas.gr>
2025-12-22 18:29:27 +0200

    Update prisma.ts

--- server/src/utils/prisma.ts
+++ server/src/utils/prisma.ts
@@ -1,11 +1,9 @@
 import { PrismaClient } from '@prisma/client';
 
+// Passing 'log' options to satisfy "non-empty options" requirement if needed,
+// and avoiding 'datasources' TS error by relying on env var.
 const prisma = new PrismaClient({
-    datasources: {
-        db: {
-            url: process.env.DATABASE_URL,
-        },
-    },
+    log: ['error', 'warn'],
 });
 
 export default prisma;
