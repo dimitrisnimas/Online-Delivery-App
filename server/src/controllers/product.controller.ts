d575657d597bca6e3767e09a1fbe88b077d5a29e
Dimitris Nimas <hello@dimitrisnimas.gr>
2025-12-22 18:24:09 +0200

    Update product.controller.ts

--- server/src/controllers/product.controller.ts
+++ server/src/controllers/product.controller.ts
@@ -1,7 +1,5 @@
 import { Request, Response } from 'express';
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import prisma from '../utils/prisma';
 
 // @desc    Get all products for current store
 // @route   GET /api/products
