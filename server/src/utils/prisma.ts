85ada6ab9767c61aea2d781e12566bdddc2afda1
Dimitris Nimas <hello@dimitrisnimas.gr>
2025-12-22 19:59:18 +0200

    Downgrade to Prisma 5 for Render compatibility

--- server/src/utils/prisma.ts
+++ server/src/utils/prisma.ts
@@ -1,27 +1,7 @@
-import 'dotenv/config';
 import { PrismaClient } from '@prisma/client';
-import { Pool } from 'pg';
-import { PrismaPg } from '@prisma/adapter-pg';
 
-let prisma: PrismaClient;
+const prisma = new PrismaClient({
+    log: ['error', 'warn'],
+});
 
-function getPrismaClient() {
-    if (!prisma) {
-        const connectionString = process.env.DATABASE_URL;
-
-        if (!connectionString) {
-            throw new Error('DATABASE_URL environment variable is not set');
-        }
-
-        const pool = new Pool({ connectionString });
-        const adapter = new PrismaPg(pool);
-
-        prisma = new PrismaClient({
-            adapter,
-            log: ['error', 'warn'],
-        });
-    }
-    return prisma;
-}
-
-export default getPrismaClient();
+export default prisma;
