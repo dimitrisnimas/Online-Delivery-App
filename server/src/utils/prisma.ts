0081cf6b9b8b0c5adc104ba442ed6946ff0d9a1c
Dimitris Nimas <hello@dimitrisnimas.gr>
2025-12-22 19:52:20 +0200

    Add dotenv to prisma.ts for DATABASE_URL availability

--- server/src/utils/prisma.ts
+++ server/src/utils/prisma.ts
@@ -1,8 +1,13 @@
+import 'dotenv/config';
 import { PrismaClient } from '@prisma/client';
 import { Pool } from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
 
 const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+    throw new Error('DATABASE_URL environment variable is not set');
+}
 
 const pool = new Pool({ connectionString });
 const adapter = new PrismaPg(pool);
