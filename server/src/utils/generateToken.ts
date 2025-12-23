20c7cbd562e8ff69d4dc4ed00e300b9bb7b850ed
Dimitris Nimas <hello@dimitrisnimas.gr>
2025-12-23 15:04:46 +0200

    Generate Token based on UserRole

--- server/src/utils/generateToken.ts
+++ server/src/utils/generateToken.ts
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 
-const generateToken = (id: string) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET || 'fallback_secret', {
+const generateToken = (id: string, role: string, storeId: string | null, isSuperAdmin: boolean = false) => {
+    return jwt.sign({ id, role, storeId, isSuperAdmin }, process.env.JWT_SECRET || 'fallback_secret', {
         expiresIn: '30d',
     });
 };
